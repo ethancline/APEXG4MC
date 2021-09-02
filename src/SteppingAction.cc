43b7e1b8b5a728f4091d59202e06d8ab86053561
David Hamilton <david.j.hamilton@glasgow.ac.uk>
2021-09-02 21:38:07 +0100

    Added multiple sensitive detectors

--- src/SteppingAction.cc
+++ src/SteppingAction.cc
@@ -27,15 +27,17 @@ void SteppingAction::UserSteppingAction(const G4Step* aStep)
   G4StepPoint* prePoint = aStep->GetPreStepPoint();
   G4StepPoint* endPoint = aStep->GetPostStepPoint();
 
-  for(int i=0; i<detector->GetNoSD(); i++) {
+  for(int i=0; i<=detector->GetNoSD(); i++) {
 
     if ( prePoint->GetTouchableHandle()->GetVolume() != detector->GetDetVol(i) &&
 	 endPoint->GetTouchableHandle()->GetVolume() == detector->GetDetVol(i) )
       {
-      FluxSD* fluxSD = (FluxSD*)detector->GetFluxSD();
-      if(fluxSD) 
-	fluxSD->ProcessHits_constStep(aStep,NULL);
+	
+	FluxSD* fluxSD = (FluxSD*)detector->GetFluxSD();
+	if(fluxSD) 
+	  fluxSD->ProcessHits_constStep(aStep,NULL);
       }
+
   }
 }
 
